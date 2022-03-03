aede24d771718e045847d8a13947b7363e5422aa
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-03-03 13:50:11 +0500

    thursday

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,1 +1,11 @@
 console.log('Dashboard')
+
+const tables = document.querySelector('.table');
+tables.addEventListener('click',()=>{
+    const bootstrap = document.querySelector('.bootstrap-drop');
+    const chevron = document.querySelector('.fa-chevron-right')
+    chevron.classList.toggle('rotate')
+    bootstrap.classList.toggle('bootstrap-show')
+    // bootstrap.style.paddingLeft='20px'
+    console.log('clicked')
+})
