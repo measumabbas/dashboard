f271a15d02b75e87c6dec28a6f814c94bf10a2da
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-03-03 14:47:06 +0500

    side bar created

--- dashboard/app.js
+++ dashboard/app.js
@@ -9,3 +9,20 @@ tables.addEventListener('click',()=>{
     // bootstrap.style.paddingLeft='20px'
     console.log('clicked')
 })
+// page-drop-show
+const page = document.querySelector('.page');
+// console.log(page);
+page.addEventListener('click',()=>{
+    const pageDown = document.querySelector('.page-drop')
+    pageDown.classList.toggle('page-drop-show')
+    const pageChevron = document.querySelector('.page-chevron')
+    pageChevron.classList.toggle('rotate')
+})
+
+const component = document.querySelector('.component')
+    component.addEventListener('click',()=>{
+    const pageDown = document.querySelector('.component-drop')
+    pageDown.classList.toggle('component-drop-show')
+    const pageChevron = document.querySelector('.component-chevron')
+    pageChevron.classList.toggle('rotate')
+})
