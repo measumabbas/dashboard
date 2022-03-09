5c758e3cffba894888aa4f6ffb4407b2610bf149
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-03-09 11:24:38 +0500

    dashboard completed

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,16 +1,16 @@
 
 const tables = document.querySelector('.table');
-tables.addEventListener('click',()=>{
+tables.addEventListener('click', () => {
     const bootstrap = document.querySelector('.bootstrap-drop');
     const chevron = document.querySelector('.fa-chevron-right')
     chevron.classList.toggle('rotate')
     bootstrap.classList.toggle('bootstrap-show')
-  
+
 })
 
 const page = document.querySelector('.page');
 
-page.addEventListener('click',()=>{
+page.addEventListener('click', () => {
     const pageDown = document.querySelector('.page-drop')
     pageDown.classList.toggle('page-drop-show')
     const pageChevron = document.querySelector('.page-chevron')
@@ -18,7 +18,7 @@ page.addEventListener('click',()=>{
 })
 
 const component = document.querySelector('.component')
-    component.addEventListener('click',()=>{
+component.addEventListener('click', () => {
     const pageDown = document.querySelector('.component-drop')
     pageDown.classList.toggle('component-drop-show')
     const pageChevron = document.querySelector('.component-chevron')
@@ -33,14 +33,14 @@ const profile = document.querySelector('.profile');
 const profile2 = document.querySelector('.profile2')
 
 
-bell.addEventListener('click',()=>{
-    if(notifications.classList.contains('show')){
+bell.addEventListener('click', () => {
+    if (notifications.classList.contains('show')) {
         notifications.classList.remove('show')
     }
-    else{
+    else {
         notifications.classList.add('show')
-        document.querySelector('.dot').style.display='none';
-        if(profile2.classList.contains('show2')){
+        document.querySelector('.dot').style.display = 'none';
+        if (profile2.classList.contains('show2')) {
             profile2.classList.remove('show2')
         }
     }
@@ -49,14 +49,14 @@ bell.addEventListener('click',()=>{
 
 
 
-profile.addEventListener('click',()=>{
+profile.addEventListener('click', () => {
 
-    if(profile2.classList.contains('show2')){
+    if (profile2.classList.contains('show2')) {
         profile2.classList.remove('show2')
     }
-    else{
+    else {
         profile2.classList.add('show2')
-        if(notifications.classList.contains('show')){
+        if (notifications.classList.contains('show')) {
             notifications.classList.remove('show')
         }
     }
@@ -73,7 +73,7 @@ profile.addEventListener('click',()=>{
 const newTask = document.querySelector('.new-task')
 const newTaskDrop = document.querySelector('.new-task-drop')
 
-newTask.addEventListener('click',()=>{
+newTask.addEventListener('click', () => {
     newTaskDrop.classList.toggle('show3')
 })
 
@@ -82,17 +82,21 @@ newTask.addEventListener('click',()=>{
 
 const hamburger = document.querySelector('.fa-bars');
 const sideBar = document.querySelector('.side-bar');
-hamburger.addEventListener('click',()=>{
-    
-    if(!sideBar.hasAttribute('style')){
-        // sideBar.removeAttribute('style')
-        sideBar.setAttribute('style','top:0px')
-        // console.log('no style attribute')
+hamburger.addEventListener('click', () => {
+
+    if (!sideBar.hasAttribute('style')) {
+        sideBar.setAttribute('style', 'top:0px')
     }
-    
+
     else {
         sideBar.removeAttribute('style')
-        // console.log('has style attribute')
     }
-    
+
+})
+
+
+const cross = document.querySelector('.cross-icon');
+
+cross.addEventListener('click', () => {
+    sideBar.removeAttribute('style');
 })
