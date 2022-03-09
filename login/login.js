5f64fb7f34dd3cfae2b45d40b1ab5c4ca334d1a8
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-03-09 14:33:06 +0500

    validation done

---
+++ login/login.js
@@ -0,0 +1,38 @@
+
+
+document.querySelector('#email').addEventListener('input',validateEmail);
+document.querySelector('#password').addEventListener('input', validatePassword);
+const emailInput = document.querySelector('.email-input');
+const passwordInput = document.querySelector('.pass-input');
+const login = document.querySelector('#submit')
+login.disabled=true;
+
+function validateEmail(){
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  
+  if(!re.test(email.value)){
+    emailInput.style.borderColor = 'red';
+    login.disabled=true;
+  } else {
+    emailInput.style.borderColor = '#fff';
+    
+    
+  }
+  
+    
+  }
+  function validatePassword(){
+    const password = document.getElementById('password');
+    
+    if(password.value.length < 7){
+      passwordInput.style.borderColor = 'red';
+      login.disabled=true;
+    }
+    else{
+      passwordInput.style.borderColor = '#fff';
+      login.disabled=false;
+      
+    }
+  
+}
---
+++ signup/signup.js
@@ -0,0 +1,72 @@
+
+
+document.querySelector('#email').addEventListener('input', validateEmail);
+document.querySelector('#name').addEventListener('input', validateName);
+document.querySelector('#password').addEventListener('input', validatePassword);
+document.querySelector('#cpassword').addEventListener('input', validatecPassword);
+const emailInput = document.querySelector('.email-input');
+const passwordInput = document.querySelector('.pass-input');
+const login = document.querySelector('#submit')
+
+
+login.disabled = true;
+
+
+function validateEmail() {
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if (!re.test(email.value)) {
+        emailInput.style.borderColor = 'red';
+        login.disabled = true;
+    } else {
+        emailInput.style.borderColor = '#fff';
+    }
+
+}
+
+
+function validatePassword() {
+    const password = document.getElementById('password');
+    const passwordInput = document.querySelector('.pass-input')
+
+    if (password.value.length < 7) {
+        passwordInput.style.borderColor = 'red';
+        login.disabled = true;
+    }
+    else {
+        passwordInput.style.borderColor = '#fff';
+
+    }
+}
+function validatecPassword() {
+    const password = document.getElementById('cpassword');
+    const passwordInput = document.querySelector('.cpass-input')
+    const passValue = document.querySelector('#password').value;
+
+    if (password.value !== passValue) {
+        passwordInput.style.borderColor = 'red';
+        login.disabled=true;
+    }
+    else {
+        console.log(passValue)
+        passwordInput.style.borderColor = '#fff';
+        login.disabled = false;
+    }
+}
+function validateName() {
+    const name = document.getElementById('name');
+    const nameInput = document.querySelector('.name-input')
+
+    if (name.value.length < 7) {
+        nameInput.style.borderColor = 'red';
+        login.disabled = true;
+    }
+    else {
+        nameInput.style.borderColor = '#fff';
+        
+
+    }
+}
+
+
